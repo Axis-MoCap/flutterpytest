81382fe1d2b220eb8f7cbcd297302955d6f2638b
Osirumiji Ihude <ihudeosirumiji@gmail.com>
2025-04-23 13:45:59 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/pyfluttertest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pyfluttertest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/flutter_ui/script_runner.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class ScriptRunner extends StatefulWidget {
+  const ScriptRunner({super.key});
+
+  @override
+  State<ScriptRunner> createState() => _ScriptRunnerState();
+}
+
+class _ScriptRunnerState extends State<ScriptRunner> {
+  String _output = '';
+
+  Future<void> _runPythonScript() async {
+    setState(() {
+      _output = 'Running script...';
+    });
+
+    try {
+      final result = await Process.run(
+          'python3', ['${Directory.current.path}/python_scripts/mocap.py']);
+
+      setState(() {
+        _output = result.stdout.toString().trim();
+        if (_output.isEmpty && result.stderr.toString().isNotEmpty) {
+          _output = 'Error: ${result.stderr}';
+        }
+      });
+    } catch (e) {
+      setState(() {
+        _output = 'Error: $e';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: _runPythonScript,
+          child: const Text('Run Python Script'),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          _output,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
